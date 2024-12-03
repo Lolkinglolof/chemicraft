6c9191ec0fb8a837a4e7753976ab8622f3b30282
Patrick <63421519+RexStarGame@users.noreply.github.com>
2024-12-03 09:49:22 +0100

    update
    .....

--- chemicraft/Assets/C#/ChemiMenuScript/ItemData.cs
+++ chemicraft/Assets/C#/ChemiMenuScript/ItemData.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ChemistryMenu : MonoBehaviour
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
 {
+    public string itemName;
+    public Sprite icon;
+    [TextArea]
+    public string description;
+
+    public List<ItemData> items = new();   
     
     // Start is called before the first frame update
     void Start()
--- chemicraft/Assets/C#/ChemiMenuScript/test.cs
+++ chemicraft/Assets/C#/ChemiMenuScript/test.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChemistryMenu : MonoBehaviour
+public class test : MonoBehaviour
 {
+    public ItemData itemtype;
+
     
     // Start is called before the first frame update
     void Start()
