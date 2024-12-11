992c6a34f04d423f1d77e0b259ff17d83f936d66
Patrick <63421519+RexStarGame@users.noreply.github.com>
2024-12-11 09:08:03 +0100

    YAY
    d

--- chemicraft/Assets/C#/ChemiMenuScript/test.cs
+++ chemicraft/Assets/C#/ChemiMenuScript/test.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class MixingSytem : MonoBehaviour
 {
     public ItemData itemtype;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer bottlesprite;
+    [SerializeField] private Inventory Inventory;
+
+    // alle objecter starter med color hvid. 
+    private Color currentColor = Color.white;
+
+    public void MixItems()
     {
-        
+        if (Inventory == null || Inventory.items.Count == 0)
+        {
+            Debug.Log("inventroy er tomt eller ikke tilsluttet");
+            return;
+        }
+        //reset color
+        currentColor = Color.white;
+        foreach (var item in Inventory.items)
+        {
+            if (item.name != null && item.items.Count > 0)
+            {
+                foreach (var item2 in item.items)
+                {
+                    //currentColor = Color.Lerp(currentColor, GetItemColor(subItem), 0.5f);
+                }
+            }
+            else
+            {
+               //currentColor = Color.Lerp(currentColor, GetItemColor(SubItem), 0.5f);
+            }
+        }
+
+       
+
+    }
+    public void GetItemColor(ItemData item)
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
